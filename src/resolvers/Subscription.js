9a77b95f4ff069f62512db75b520f9ccd6a83d64
Halil Azyikmis <37673859+hazyikmis@users.noreply.github.com>
2020-04-23 00:28:28 +0200

    subscriptions fixed

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,20 +1,5 @@
 const Subscription = {
-  count: {
-    //subscribe(parent, args, ctx, info) {
-    subscribe(parent, args, { pubsub }, info) {
-      let counter = 0;
-
-      setInterval(() => {
-        counter++;
-        pubsub.publish("countChannel", {
-          count: counter,
-        });
-      }, 1000);
-
-      return pubsub.asyncIterator("countChannel");
-    },
-  },
-
+  /*
   comment: {
     subscribe(parent, { postId }, { db, pubsub }, info) {
       const post = db.posts.find(
@@ -30,13 +15,59 @@ const Subscription = {
       //publishing is done when new comment created (check Mutation resolver)
     },
   },
+  */
+  comment: {
+    subscribe(parent, args, { prisma }, info) {
+      //data flow: Prisma --> NodeJS --> Client (GraphQL Playground)
+      //schema.graphql has changed to understand NodeJS & GraphQL each other better
+      //for example, the returning data in the CommentSubscriptionPayload, its name changed to "node",
+      //because its named "node" in the GraphQL !!!
 
+      //without operation argumnets we subscribe all comments (without "where")
+      //return prisma.subscription.comment(null, info);
+
+      //its possible to subscribe only for DELETE mutations --> "mutation_in"
+      //or change of specific field(s) --> "updateFields_contains"
+      //if we want to subscribe for specific data rather than the aboves, we nned to use "node"
+      return prisma.subscription.comment(
+        {
+          where: {
+            node: {
+              post: {
+                id: parseInt(args.postId),
+              },
+            },
+          },
+        },
+        info
+      );
+    },
+  },
+
+  /*
   post: {
     //whenever new post created this resolver method below sends it to the "posts" channel
     //be careful: no args, means all posts (from all authors)
     //be careful 2: cheking the publishing is true or false is done in createPost resolver method!
     subscribe(parent, args, { pubsub }, info) {
       return pubsub.asyncIterator("post");
+    },
+  },
+*/
+
+  post: {
+    subscribe(parent, args, { prisma }, info) {
+      return prisma.subscription.post(
+        {
+          where: {
+            node: {
+              published: true,
+            },
+            mutation_in: ["CREATED"],
+          },
+        },
+        info
+      );
     },
   },
 };
